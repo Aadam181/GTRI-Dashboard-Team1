225b7ab79bff41f0da76e45b028627cb6cae6ac8
Aadam181 <aadam181@students.kennesaw.edu>
2022-02-09 21:12:28 -0500

    Starting modifying the layout/design of the main landing page. I added the side nav bar and logo and also changed the color of the existing top toolbar.

--- GTRIDashboard-App-Team1/src/app/app.module.ts
+++ GTRIDashboard-App-Team1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketStatsComponent } from './ticket-stats/ticket-stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketStatsComponent
   ],
   imports: [
     BrowserModule,
---
+++ GTRIDashboard-App-Team1/src/app/ticket-stats/ticket-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-stats',
+  templateUrl: './ticket-stats.component.html',
+  styleUrls: ['./ticket-stats.component.css']
+})
+export class TicketStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
