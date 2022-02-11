8681bea52e9192298972641500a98962d1a52461
Sally <svigil@students.kennesaw.edu>
2022-02-10 20:50:10 -0500

    style update

--- GTRIDashboard-App-Team1/src/app/app.module.ts
+++ GTRIDashboard-App-Team1/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TicketStatsComponent } from './ticket-stats/ticket-stats.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { TicketStatsComponent } from './ticket-stats/ticket-stats.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
