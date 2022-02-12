02426aabdc4141a0d8593b41edd639cd6aa450d9
Aadam181 <aadam181@students.kennesaw.edu>
2022-02-12 13:47:25 -0500

    Forgot to remove the TicketStatistics import. Will re-add later.

--- GTRIDashboard-App-Team1/src/app/app.module.ts
+++ GTRIDashboard-App-Team1/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TicketStatsComponent } from './ticket-stats/ticket-stats.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,7 +16,6 @@ import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.componen
 @NgModule({
   declarations: [
     AppComponent,
-    TicketStatsComponent,
     TopnavbarComponent,
     SidenavbarComponent,
   ],
