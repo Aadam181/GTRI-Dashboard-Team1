8da71cd8e92061314096612a3c9a14f697fcdf02
Sally <svigil@students.kennesaw.edu>
2022-02-15 11:22:35 -0500

    Fixed merging errors

--- GTRIDashboard-App-Team1/src/app/app-routing.module.ts
+++ GTRIDashboard-App-Team1/src/app/app-routing.module.ts
@@ -3,18 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AssetStatisticsComponent } from './components/asset-statistics/asset-statistics.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
-
-const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch:'full'},
-  {path: 'dashboard', component:DashboardComponent},
-  {path: 'login', component:LoginComponent}
 import { TicketStatisticsComponent } from './components/ticket-statistics/ticket-statistics.component';
+
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch:'full'},
   {path: 'dashboard', component:DashboardComponent},
   {path: 'asset-statistics', component:AssetStatisticsComponent},
-  {path: 'ticket-statistics', component:TicketStatisticsComponent}
+  {path: 'ticket-statistics', component:TicketStatisticsComponent},
+  {path: 'login', component:LoginComponent}
 ];
 
 @NgModule({
