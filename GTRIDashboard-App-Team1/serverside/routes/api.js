8a6b68783d008b147d789e2e3fb05609482f45cf
Sally <svigil@students.kennesaw.edu>
2022-03-16 16:47:41 -0400

    checks encrypted passwords when logging in

--- GTRIDashboard-App-Team1/serverside/routes/api.js
+++ GTRIDashboard-App-Team1/serverside/routes/api.js
@@ -1,58 +1,56 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 const User = require('../models/user')
 const mongoose = require('mongoose')
 
 const db = "mongodb+srv://capstone:capstone@cluster0.nx9oh.mongodb.net/capstoneproject"
 
 
-mongoose.connect(db, err =>{
-    if (err){
+mongoose.connect(db, err => {
+    if (err) {
         console.error("error" + err)
-    }else {
+    } else {
         console.log('connected')
     }
 })
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.send('from API route')
 })
 
-router.post('/register', (req, res)=>{
+router.post('/register', (req, res) => {
     let userData = req.body
     let user = new User(userData)
-    user.save((error, registeredUser)=>{
-        if (error){
+    user.save((error, registeredUser) => {
+        if (error) {
             console.log(error)
         } else {
-            let payload = { subject: registeredUser._id} 
+            let payload = { subject: registeredUser._id }
             let token = jwt.sign(payload, 'secretKey')
-            res.status(200).send({token})
+            res.status(200).send({ token })
         }
     })
 })
 
-router.post('/login', (req, res)=>{
-    let userData = req.body
 
-    User.findOne({email: userData.email}, (error, user)=>{
-        if (error){
-            console.log(error)
+router.post("/login", async (req, res) => {
+    const userData = req.body;
+    const user = await User.findOne({ email: userData.email });
+    if (user) {
+        // check user password with hashed password stored in the database
+        const validPassword = await bcrypt.compare(userData.password, user.password);
+        if (validPassword) {
+            let payload = { subject: user._id }
+            let token = jwt.sign(payload, 'secretKey')
+            res.status(200).json({ message: "Valid password", token });
         } else {
-            if (!user){
-                res.status(401).send('Invalid email')
-            } else 
-            if ( user.password !== userData.password){
-                res.status(401).send('Invalid password')
-            } else {
-                let payload = { subject: user._id}
-                let token = jwt.sign(payload, 'secretKey')
-                res.status(200).send({token})
-            }
+            res.status(400).json({ error: "Invalid Password" });
         }
-    })
-})
-
+    } else {
+        res.status(401).json({ error: "User does not exist" });
+    }
+});
 
 module.exports = router
