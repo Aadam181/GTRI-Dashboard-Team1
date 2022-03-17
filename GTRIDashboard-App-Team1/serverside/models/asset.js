adbc4112cd26d3380985e47e6c174266b26e6de7
ashtonhowell <99498428+ashtonhowell@users.noreply.github.com>
2022-03-17 12:57:14 -0400

    ashton

---
+++ GTRIDashboard-App-Team1/serverside/models/asset.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const Schema = mongoose.Schema
+const assetSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    cpassword: String
+
+})
+
+userSchema.pre('save', function (next) {
+    if (this.isModified('password')) {
+        bcrypt.hash(this.password, 10, (err, hash) => {
+            if (err) return next(err);
+            this.password = hash;
+            next();
+        }
+        )
+    }
+})
+
+userSchema.pre('save', function (next) {
+    if (this.isModified('cpassword')) {
+        bcrypt.hash(this.cpassword, 10, (err, hash) => {
+            if (err) return next(err);
+            this.cpassword = hash;
+            next();
+        }
+        )
+    }
+})
+
+module.exports = mongoose.model('asset', userSchema, 'assets')
