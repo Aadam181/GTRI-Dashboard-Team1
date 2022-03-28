692440b1dbb46e8266f238d1f700cb813ff04642
ashtonhowell <99498428+ashtonhowell@users.noreply.github.com>
2022-03-28 13:47:01 -0400

    alter assets

--- GTRIDashboard-App-Team1/serverside/models/asset.js
+++ GTRIDashboard-App-Team1/serverside/models/asset.js
@@ -8,26 +8,6 @@ const assetSchema = new Schema({
 
 })
 
-assetSchema.pre('save', function (next) {
-    if (this.isModified('password')) {
-        bcrypt.hash(this.password, 10, (err, hash) => {
-            if (err) return next(err);
-            this.password = hash;
-            next();
-        }
-        )
-    }
-})
 
-assetSchema.pre('save', function (next) {
-    if (this.isModified('cpassword')) {
-        bcrypt.hash(this.cpassword, 10, (err, hash) => {
-            if (err) return next(err);
-            this.cpassword = hash;
-            next();
-        }
-        )
-    }
-})
 
 module.exports = mongoose.model('asset', userSchema, 'assets')
